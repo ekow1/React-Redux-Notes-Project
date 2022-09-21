a1d38c9af7febcc450e3eef330bb44f586e623d5
ekow <ekowfirmino@yahoo.com>
2022-09-21 12:29:48 +0000

    updated scroll bar

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -16,12 +16,12 @@ function CardDetails({ note }) {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none "
             index={note.id}
           >
             <div className="relative lg:w-5/12 md:5/12 w-80 my-6 mx-auto  ">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col justify-center   bg-yellow-400 outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col justify-center lg:h-96  overflow-x-hidden overflow-y-auto  bg-yellow-400 outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 bg-emerald-600 border-b border-solid border-yellow-400 ">
                   <h3 className="text-3xl font-semibold text-yellow-400">
